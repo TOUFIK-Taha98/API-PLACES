ad57de537acad1303c16a499b72e1a18278b039b
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-05-27 11:16:40 +0200

    Get a place with id (without mongo)

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const router = express.Router();
 
+const DUMMY_PLACES = [
+    {
+        id: 'p1', 
+        title :'Empire State Building',
+        description : 'One of the most famous sky scrapers in the World!',
+        location : {
+            lat : 40.7484474,
+            lng: -73.9871516
+        },
+        address : '20 W 34th St, New York, NY 10001',
+        creator: 'u1'
+    }
+]
+
 router.get('/', (req, res, next) => {
     console.log('GET REQUEST in Places');
     res.json({message: "IT WORKS!"})
+})
+router.get('/:id', (req, res, next) => {
+    const placeId = req.params.id;
+    const place = DUMMY_PLACES.find(p => p.id === placeId)
+    res.json(place)
 })
 
 module.exports = router;
