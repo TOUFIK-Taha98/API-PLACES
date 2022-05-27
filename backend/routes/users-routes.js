2d500860311266834fe7f53b5b1e060aab26ad87
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-05-27 15:32:31 +0200

    Implementing routes : get all users & signup & login without validating inputs

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -6,7 +6,13 @@ const router = express.Router();
 const usersControllers = require('../controllers/users-controllers')
 
 
+router.get('/', usersControllers.getUsers)
+
 router.get('/:id', usersControllers.getUserById)
+
+router.post('/signup', usersControllers.signup)
+
+router.post('/login', usersControllers.login)
 
 
 module.exports = router;
