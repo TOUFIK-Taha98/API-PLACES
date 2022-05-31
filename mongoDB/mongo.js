23606aedb7ed4196b71c116868c35a19164141ee
TOUFIK Taha DEF <ZZNR5411@orange.com>
2022-05-31 17:26:06 +0200

    Adding connexion to the database

--- mongoDB/mongo.js
+++ mongoDB/mongo.js
@@ -1,10 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
-require('dotenv').config();
-
-const URL_DB = process.env.URL_DB;
+require('dotenv').config({ path: './.env' });
+const MONGO_PASS = encodeURIComponent(process.env.password);
+const MONGO_USER = encodeURIComponent(process.env.user);
+const cluster = encodeURIComponent(process.env.cluster);
+const MONGO_DATABASE = encodeURIComponent(process.env.db_name);
+const URL_DB = `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@${cluster}.osxrutg.mongodb.net/${MONGO_DATABASE}?retryWrites=true&w=majority`;
 
 const createProduct = async ( req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price
+    };
 
+    const client = new MongoClient(URL_DB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    try {
+        await client.connect(); // Se connecter à la BD 
+        console.log('Connected correctly to server');
+        const db = client.db(); // Récupérer la BD
+        console.log(db);
+        const result = db.collection('products').insertOne(newProduct); // Inserer dans la BD
+
+    } catch (error) {
+        return res.json({message: error.message})
+    }
+    client.close(); // Fermer la connexion
+
+    res.json(newProduct);
 };
 
 const getProducts = async ( req, res, next) => {
