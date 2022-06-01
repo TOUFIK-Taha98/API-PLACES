700bb931f159ade435493738d0a33792fdcb5f83
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-01 23:19:21 +0200

    Delete a place

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -150,13 +150,30 @@ const updatePlace= async (req, res, next) => {
     res.status(200).json({place : place.toObject({getters: true})});
 }
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
     const placeId = req.params.id;
-    if(!DUMMY_PLACES.find(p => p.id === placeId)){
-        throw new HttpError("Could not find the place for that id", 404);
-    }
-    DUMMY_PLACES = DUMMY_PLACES.filter(p => p.placeId !== placeId);
 
+    
+    let place;
+    try {
+        place = await Place.findById(placeId)
+    } catch (err) {
+        const error = new HttpError(
+            'Something went wrong, could not delete place',
+            500
+        );
+        return next(error);
+    }
+
+    try{
+        await place.remove();
+    }catch(err){
+        const error = new HttpError(
+            'Something went wrong, could not delete place',
+            500
+        );
+        return next(error);
+    }
     res.status(200).json({message : 'Deleted place.'})
 }
 exports.getPlaceById = getPlaceById;
