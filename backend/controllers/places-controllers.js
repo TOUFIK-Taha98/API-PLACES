73a65cea981170213214b20af569d8634bacc961
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-02 02:02:53 +0200

    Getting Places - An Alternative

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -37,9 +37,11 @@ const getPlaceById = async (req, res, next) => {
 const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.uid;
 
-    let places;
+    //let places;
+    let userWithPlaces;
     try {
-        places = await Place.find({creator: userId}).exec();
+        //places = await Place.find({creator: userId}).exec();
+        userWithPlaces = await User.findById(userId).populate('places');
     } catch (err) {
         const error = new HttpError(
             'Fetching places failed, please try again later',
@@ -48,7 +50,8 @@ const getPlacesByUserId = async (req, res, next) => {
         return next(error);
     }
     
-    if(!places || places.length === 0){
+    //if(!places || places.length === 0){
+    if(!userWithPlaces || userWithPlaces.places.length === 0){
         return next(
             new HttpError(
                 'Could not find places for the provided User Id', 
@@ -57,7 +60,7 @@ const getPlacesByUserId = async (req, res, next) => {
         );
     }
 
-    res.json({ places : places.map(place => place.toObject({getters: true}))})
+    res.json({ places : userWithPlaces.places.map(place => place.toObject({getters: true}))})
 };
 
 const addNewPlace = async (req, res, next) => {
